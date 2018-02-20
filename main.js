43645ae9c66a621cf7715fcb180cbeaf82ce8f7d
Alexandra Mirtcheva <alex@solanalabs.io>
2018-02-20 15:46:20 -0800

    connect saga middleware to store

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
-import "babel-polyfill"
+import 'babel-polyfill'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+import { helloSaga } from './sagas'
 
 import Counter from './Counter'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(reducer, applyMiddleware(sagaMiddleware))
 
-const action = type => store.dispatch({type})
+sagaMiddleware.run(helloSaga)
+
+const action = type => store.dispatch({ type })
 
 function render() {
   ReactDOM.render(
     <Counter
       value={store.getState()}
       onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
+      onDecrement={() => action('DECREMENT')}
+    />,
     document.getElementById('root')
   )
 }
